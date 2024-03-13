18369f61af9a78870f54110125912f615c4056c8
Alisha M Irfan <160208713+Alisha000111@users.noreply.github.com>
2024-03-13 14:08:03 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,16 @@
+//let n1 = 10
+//let n2 = 5
+//console.log(n1 == 10 && n2 == 5);
+//let n1 = 10
+//let n2 = 5
+//console.log(n1 == 10 || n2 == n1);
+//let n1 = 10
+//let n2 = 5
+//console.log(!(n1 == n2))
+let n1: string = "alisha"
+if(n1 == "alisha"){
+    console.log("name is alisha");
+}
+else{
+    console.log("name is not alisha");
+}
